a74083049e9e4f1ff99bf468ccb40b46953e4780
Ankit <ankithooda23@gmail.com>
2024-02-02 20:01:38 +0530

    Added user program to test Lottery scheduler

--- loopa.c
+++ loopa.c
@@ -2,10 +2,27 @@
 #include "user.h"
 #include "stat.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   settickets(30);
+  int cpid = fork();
+
+  if (cpid == 0) {
+    settickets(20);
+
+    int gpid = fork();
+
+    if (gpid == 0) {
+      settickets(10);
+    } else if (gpid < 0) {
+      printf(1, "Error in spawning grandchild\n");
+    }
+  } else if (cpid < 0) {
+    printf(1, "Error in spawning grandchild\n");
+  }
+
   for (;;) {;}
+
   exit();
+  return 0;
 }
